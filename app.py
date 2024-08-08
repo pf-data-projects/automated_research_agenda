a03d72df405b32c7dc740f515a7384b685f03238
Jeremy Simons <jeremy.simons@publicfirst.co.uk>
2024-08-08 17:26:53 +0100

    Add caching

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ mde = Mde(app)
 # Replace with your OpenAI API key
 OPENAI_API_KEY = os.environ['OpenAI']
 
+# In-memory cache for markdown content
+markdown_cache = []
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/get_response', methods=['POST'])
 def get_response():
@@ -25,13 +30,33 @@ def get_response():
     response = call_openai_api(prompt, extra_instructions)
     return jsonify({'response': response})
 
+
 @app.route('/save_markdown', methods=['POST'])
 def save_markdown():
-    data = request.form['markdown_content']
-    html_content = markdown.markdown(data)
+    markdown_content = request.form['markdown_content']
+    markdown_cache.append(markdown_content)
+    markdown_payload = ""
+    for item in markdown_cache:
+        markdown_payload += '\n' + item
+    html_content = markdown.markdown(markdown_payload)
     return jsonify({'html_content': html_content})
 
+
+@app.route('/get_cached_markdown', methods=['GET'])
+def get_cached_markdown():
+    concatenated_markdown = '\n'.join(markdown_cache)
+    return jsonify({'cached_markdown': concatenated_markdown})
+
+def get_markdown_internal():
+    markdown_list = markdown_cache
+    markdown_payload = ""
+    for item in markdown_list:
+        markdown_payload += '\n' + item
+    return markdown_payload
+
 def call_openai_api(prompt, extra_instructions):
+    markdown = get_markdown_internal()
+    print(markdown)
     headers = {
         'Authorization': f'Bearer {OPENAI_API_KEY}',
         'Content-Type': 'application/json'
@@ -47,6 +72,7 @@ def call_openai_api(prompt, extra_instructions):
     if response.status_code == 200:
         return response.json().get('choices', [{}])[0].get('message', {}).get('content', '')
     return 'Error contacting OpenAI API'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
